4ed149b402530edcc680335ceca4880da5b60b91
mavdjiev <radek.von.boktor@gmail.com>
2017-05-03 15:45:27 +0300

    ex2_Ready

--- src/Homework/ex2.java
+++ src/Homework/ex2.java
@@ -1,6 +1,6 @@
 package Homework;
 
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,12 +8,22 @@ import java.util.Scanner;
  */
 public class ex2 {
     public static void main(String[] args) {
+        //Initializing an array of 5(can be more) values
         int[] arr = new  int[5];
+        //Initializing the scanner
         Scanner reader = new Scanner(System.in);
         System.out.println("Please enter 5 random values: ");
+        //Storing the user entered data in the array
         for(int i = 0; i < 5; i++){
             arr[i] = reader.nextInt();
         }
+        //Sorting the data in the arrays
+        Arrays.sort(arr);
+        System.out.println("Min value: " + arr[0]);
+        System.out.println("Max value: " + arr[arr.length - 1]);
+
+
+
 
     }
 }
