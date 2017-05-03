47d92fcd9b7d2fe5d55943b5e0f91c30b7e6df99
Mars Avdjiev <mavdjiev@softserveinc.com>
2017-05-03 15:08:26 +0300

    Initial commit

---
+++ src/Homework/ex1.java
@@ -0,0 +1,10 @@
+package Homework;
+
+/**
+ * Write a Java program that defines an array with integer numbers. The program should print the sum of all array elements
+ */
+public class ex1 {
+    public static void main(String[] args) {
+
+    }
+}
