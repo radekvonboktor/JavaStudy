fee86f81e88526cc14b4c4596a204992b50fce8c
mavdjiev <radek.von.boktor@gmail.com>
2017-05-03 17:01:09 +0300

    added custom getMinValue/getMaxValue methods

--- src/homework/Ex2.java
+++ src/homework/Ex2.java
@@ -17,13 +17,38 @@ public class Ex2 {
         for(int i = 0; i < 5; i++){
             arr[i] = reader.nextInt();
         }
-        //Sorting the data in the arrays
-        Arrays.sort(arr);
-        System.out.println("Min value: " + arr[0]);
-        System.out.println("Max value: " + arr[arr.length - 1]);
+        //Creating another array so that we can use it for another way to do the task
+        int[] arr2 = arr;
+        //Sorting the data in the arrays using Arrays.sort()
+        Arrays.sort(arr2);
+        System.out.println("Using Arrays.sort()");
+        System.out.println("Min value: " + arr2[0]);
+        System.out.println("Max value: " + arr2[arr2.length - 1]);
 
+        //Using the created methods in order to complete the task
+        System.out.println("Using custom methods to complete the task");
+        System.out.println("Min value: " + getMinValue(arr));
+        System.out.println("Max value: " + getMaxValue(arr));
+    }
 
+    //Creating getMaxValue and getMinValue methods
+    public static int getMaxValue(int[] arr){
+        int maxValue = arr[0];
+        for(int i = 0; i < 5; i++){
+            if(arr[i] > maxValue){
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
 
-
+    public static int getMinValue(int[] arr){
+        int minValue = arr[0];
+        for(int i = 0; i < 5; i++){
+            if(arr[i] < minValue){
+                minValue = arr[i];
+            }
+        }
+        return minValue;
     }
 }
