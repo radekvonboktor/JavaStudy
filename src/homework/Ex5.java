2a202e41563aeb25ce60e72e8517bc878dbe4506
mavdjiev <radek.von.boktor@gmail.com>
2017-05-05 15:47:02 +0300

    example 5

---
+++ src/homework/Ex5.java
@@ -0,0 +1,21 @@
+package homework;
+
+import java.util.Scanner;
+
+/**
+ * Write a program that shows on the screen if a number is prime or not
+ */
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter a random number: ");
+        int rand = reader.nextInt();
+        if(rand % 2 != 0){
+            System.out.println("The number you entered is prime");
+        } else if(rand == 2){
+            System.out.println("The number you entered is prime");
+        } else {
+            System.out.println("The number you entered is NOT PRIME YOU DUMBFUCK");
+        }
+    }
+}
