68e513781c49d6b096c5612e17d4b354145b792e
Mars <radek.von.boktor@gmail.com>
2017-06-05 22:56:07 +0300

    Adding Interface Items

---
+++ src/homeworkConstructors/VendingMachine/Items.java
@@ -0,0 +1,11 @@
+package homeworkConstructors.VendingMachine;
+
+/**
+ * Created by Mars on 6/5/2017.
+ */
+public interface Items {
+    void Items();
+    double price = 0;
+    double getPrice();
+
+}
